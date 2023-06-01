7a84400737440e0f244ae6846c35efc46c97c1e6
MarioSantoro <mariosantoro124@gmail.com>
2023-06-01 10:07:58 +0200

    fix default prevent on button a

--- main.js
+++ main.js
@@ -77,8 +77,8 @@ const buttonLikes = document.querySelectorAll("a.like-button");
 buttonLikes.forEach((button , index) => {
     let IsAlreadyClicked = false;
     button.style.cursor = "pointer";
-    button.addEventListener("click" , function(){
-        
+    button.addEventListener("click" , function(event){
+        event.preventDefault();
         if(!IsAlreadyClicked){
             IsAlreadyClicked = true;
             button.classList.add("color-blue");
@@ -130,7 +130,7 @@ function addPostOnDom(name , date , content , media , id , likes , imagePart){
                 <div class="post__footer">
                     <div class="likes js-likes">
                         <div class="likes__cta">
-                            <a class="like-button  js-like-button"  data-postid="1">
+                            <a class="like-button  js-like-button" href="#"  data-postid="1">
                                 <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                                 <span class="like-button__label">Mi Piace</span>
                             </a>
